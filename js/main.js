ccad97e0fdd7f706b5da3e4c70d25137999af890
Duc Hoang <30746271+minhduc09@users.noreply.github.com>
2019-09-29 18:05:53 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+    //declare numbers of like and dislike at the begining
+    var like = 100, dis = 0; 
+    //display like numbers increasing by 1
+    $("#like-button").click(function(){
+        like++;
+        $("#like").html(like);
+    });
+    //display dislike numbers decreasing by 1
+    $("#dislike-button").click(function(){
+        dis++;
+        $("#dislike").html(dis);
+    });
+    //prevent spamming with double click
+    $("button").dblclick(function(){
+        alert("Please do not spam the button!");
+    });
+   
+    $(".navbar-brand").click(function(){
+        location.reload();
+    });
+
+    $("#about-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+    $("#skills-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 1000);
+    });
+    $("#experience-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#experience").offset().top
+        }, 1000);
+    });
+    $("#projects-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 1000);
+    });
+    $("#education-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#education").offset().top
+        }, 1000);
+    });
+})
